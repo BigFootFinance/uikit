9db713d13401917cf7f268ce011a3fe6df788bd2
Lyu Jinhang <truepai415@gmail.com>
2021-04-02 21:12:20 +0800

    fix: address

--- dist/index.cjs.js
+++ dist/index.cjs.js
@@ -2491,7 +2491,7 @@ var templateObject_1$E;
 var PriceLink = styled__default['default'].a(templateObject_1$F || (templateObject_1$F = __makeTemplateObject(["\n  display: flex;\n  align-items: center;\n  svg {\n    transition: transform 0.3s;\n  }\n  :hover {\n    svg {\n      transform: scale(1.2);\n    }\n  }\n"], ["\n  display: flex;\n  align-items: center;\n  svg {\n    transition: transform 0.3s;\n  }\n  :hover {\n    svg {\n      transform: scale(1.2);\n    }\n  }\n"])));
 var CakePrice = function (_a) {
     var cakePriceUsd = _a.cakePriceUsd;
-    return cakePriceUsd ? (React__default['default'].createElement(PriceLink, { href: "https://pancakeswap.info/token/0xD26cc8F706759d7b916A7e8dE4043ef92d63816c", target: "_blank" },
+    return cakePriceUsd ? (React__default['default'].createElement(PriceLink, { href: "https://pancakeswap.info/token/0x70219E2c64F32517d4271f74445757BF7eC91AD2", target: "_blank" },
         React__default['default'].createElement(Icon$w, { width: "24px", mr: "8px" }),
         React__default['default'].createElement(Text, { color: "textSubtle", bold: true }, "$" + cakePriceUsd.toFixed(3)))) : (React__default['default'].createElement(Skeleton, { width: 80, height: 24 }));
 };
--- dist/index.esm.js
+++ dist/index.esm.js
@@ -2478,7 +2478,7 @@ var templateObject_1$E;
 var PriceLink = styled.a(templateObject_1$F || (templateObject_1$F = __makeTemplateObject(["\n  display: flex;\n  align-items: center;\n  svg {\n    transition: transform 0.3s;\n  }\n  :hover {\n    svg {\n      transform: scale(1.2);\n    }\n  }\n"], ["\n  display: flex;\n  align-items: center;\n  svg {\n    transition: transform 0.3s;\n  }\n  :hover {\n    svg {\n      transform: scale(1.2);\n    }\n  }\n"])));
 var CakePrice = function (_a) {
     var cakePriceUsd = _a.cakePriceUsd;
-    return cakePriceUsd ? (React.createElement(PriceLink, { href: "https://pancakeswap.info/token/0xD26cc8F706759d7b916A7e8dE4043ef92d63816c", target: "_blank" },
+    return cakePriceUsd ? (React.createElement(PriceLink, { href: "https://pancakeswap.info/token/0x70219E2c64F32517d4271f74445757BF7eC91AD2", target: "_blank" },
         React.createElement(Icon$w, { width: "24px", mr: "8px" }),
         React.createElement(Text, { color: "textSubtle", bold: true }, "$" + cakePriceUsd.toFixed(3)))) : (React.createElement(Skeleton, { width: 80, height: 24 }));
 };
